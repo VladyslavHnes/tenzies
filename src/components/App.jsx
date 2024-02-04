78a60e67ba97a55eaededee19bd83bcd574cdea3
vhnes <vhnes03@gmail.com>
2024-02-04 23:48:11 +0200

    Added logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { nanoid } from 'nanoid'
 import Die from './Die'
 
 function App() {
 
-  const randomNumbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
-  let dice = randomNumbers.map(num =>  <Die value={num}/>)
+  let [numbers, setNumbers] = useState(generateRandomNumbers());
+  let [isSolved, setIsSolved] = useState(false);
 
-  console.log(window.innerWidth)
+  useEffect(() => {
+    let isSolved = numbers.every(num => num.isHeld && num.value === numbers[0].value)
+    setIsSolved(isSolved)
+  }, [numbers])
+
+  function generateRandomNumbers() {
+    let numbers = Array.from({ length: 10 }, () => generateRandomNumber());
+    return numbers.map(num => ({
+      id: nanoid(),
+      value: num,
+      isHeld: false
+    }))
+  }
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  function getNumber() {
+    return ({
+      id: nanoid(),
+      value: generateRandomNumber(),
+      isHeld: false
+    })
+  }
+
+  function refreshNumbers() {
+    let newNumbers = isSolved ? generateRandomNumbers() : numbers.map(num => num.isHeld === false ? getNumber() : num)
+    setNumbers(newNumbers)
+  }
+
+  function hold(id) {
+    setNumbers(() => numbers.map(prevNum => prevNum.id === id ? {...prevNum, isHeld: !prevNum.isHeld} :  prevNum))
+  }
+
+  let dice = numbers.map(num => 
+    <Die 
+      key={num.id} 
+      id={num.id} 
+      value={num.value} 
+      isHeld={num.isHeld} 
+      holdFunc={hold}
+    />)
 
   return (
     <>
@@ -16,8 +59,9 @@ function App() {
           <div className='dice'>
             {dice}
           </div>
-          <button>New Game</button>
+          <button onClick={refreshNumbers}>{isSolved ? "New Game" : "Refresh"}</button>
       </main>
+      
     </>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function Die(props) {
     return (
-        <div className="die">
+        <div 
+            className="die" 
+            style={props.isHeld ? {backgroundColor: "#59E391"} : {backgroundColor: "#F5F5F5"}}
+            onClick={() => props.holdFunc(props.id)}
+        >
             <p>{props.value}</p>
         </div>
     )
